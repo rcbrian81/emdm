7f52705c673607b626d0be1e5d991622f043e850
Brian Sanchez <rcbriansanchez@gmail.com>
2024-10-20 23:58:52 -0700

    added new links to make up for uncrawlable navbar links.

--- src/app/components/Micheladas.js
+++ src/app/components/Micheladas.js
@@ -4,17 +4,19 @@ export default function Micheladas() {
   return (
     <div className="flex flex-col md:flex-row w-full h-auto md:h-[50vh] bg-black text-right">
       <div className="relative w-full md:w-2/5 h-[40vh] md:h-full">
-        <Image
-          src="/images/miche2.webp"
-          alt="Ice cold red/orange michelada served in a large glass Tankard cup. The cup is seating with precipitation and a resturant dining area in the background with chips and beer."
-          layout="fill"
-          objectFit="cover"
-        />
+        <a href="micheladas">
+          <Image
+            src="/images/miche2.webp"
+            alt="Ice cold red/orange michelada served in a large glass Tankard cup. The cup is seating with precipitation and a resturant dining area in the background with chips and beer."
+            layout="fill"
+            objectFit="cover"
+          />
+        </a>
       </div>
 
       <div className="flex-1 p-8 md:p-16 w-full md:w-3/5">
         <h3 className="text-3xl md:text-5xl m-4 md:m-8 text-white">
-          Micheladas
+          <a href="micheladas">Micheladas</a>
         </h3>
         <p className="text-lg md:text-2xl text-white">
           Beloved by our customers, our micheladas are a refreshing mix of
--- src/app/components/Tacos&Burritos.js
+++ src/app/components/Tacos&Burritos.js
@@ -4,7 +4,9 @@ export default function Tacos_Burritos() {
   return (
     <div className="flex flex-col md:flex-row bg-white w-full h-auto md:h-[50vh] text-black">
       <div className="p-8 md:p-16 w-full md:w-3/5">
-        <h3 className="text-3xl md:text-5xl m-4 md:m-8">Mexican Staples</h3>
+        <h3 className="text-3xl md:text-5xl m-4 md:m-8">
+          <a href="/burritos">Mexican Staples</a>
+        </h3>
         <p className="text-lg md:text-2xl">
           Our menu proudly features the beloved staples of Mexican
           cuisine—tacos, burritos, and quesadillas. Prepared with the same care
@@ -15,12 +17,14 @@ export default function Tacos_Burritos() {
       </div>
 
       <div className="relative w-full md:w-2/5 h-[40vh] md:h-full">
-        <Image
-          src="/images/tacos.webp"
-          alt="Tacos, burrito, and quesadillas all on a single table."
-          layout="fill"
-          objectFit="cover"
-        />
+        <a href="/tacos">
+          <Image
+            src="/images/tacos.webp"
+            alt="Tacos, burrito, and quesadillas all on a single table."
+            layout="fill"
+            objectFit="cover"
+          />
+        </a>
       </div>
     </div>
   );
